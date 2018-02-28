f2024227279be6dc14222044db929b9429e09ddc
yukilabo <yukilabo3@gmail.com>
2018-03-01 03:00:06 +0900

    スクレイピングツール実装中
    インデックスページから各詳細ページのURLを抽出する部分は実装済み。
    詳細ページから品名を抽出する部分は実装中。

---
+++ BunbetsuScraper/BunbetsuScraper/Program.cs
@@ -0,0 +1,114 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
+using AngleSharp.Parser.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace BunbetsuScraper
+{
+	class Program
+	{
+		private const string PAGE_URL = @"http://www.city.hiroshima.lg.jp/www/contents/1277099413287/index.html";
+
+		static void Main(string[] args)
+		{
+			var details = GetDetailUrls(PAGE_URL);
+
+			details.ForEach(t => GetBunbetsuItems(t));
+		}
+
+		static List<string> GetDetailUrls(string indexUrl)
+		{
+			var doc = GetHtmlDocument(indexUrl);
+
+			// indexページ内の各行(ア行、カ行)へのリンクを抽出
+			var items = doc.QuerySelectorAll("a[href*='#']")
+				.Select(t => t.GetAttribute("href"))
+				.Select(t => t.Split('#').First())
+				.Where(t => t.Length > 0)
+				.Distinct();
+
+			return items.ToList();
+		}
+
+		private static void GetBunbetsuItems(string detailUrl)
+		{
+			var doc = GetHtmlDocument(detailUrl);
+
+			var items = doc.QuerySelector("#Main")
+				.QuerySelectorAll("table table")[2]
+				.QuerySelectorAll("tr");
+
+			foreach (var item in items)
+			{
+				if (IsHeader(item) || IsSeparator(item))
+				{
+					continue;
+				}
+
+				var itemName = "";
+				if (IsTopItem(item))
+				{
+					itemName = item.ChildNodes[3].TextContent;
+				}
+				else if (IsNormalItem(item))
+				{
+					itemName = item.FirstElementChild.TextContent;
+				}
+				else if (IsDetailItem(item))
+				{
+					itemName = item.ChildNodes[2].TextContent;
+				}
+				else
+				{
+					itemName = item.ChildNodes[1].TextContent;
+				}
+				Console.WriteLine(itemName);
+			}
+		}
+
+		private static bool IsDetailItem(IElement item)
+		{
+			return item.ChildElementCount == 5
+				&& item.FirstElementChild.HasAttribute("rowspan")
+				&& item.FirstElementChild.TextContent.Length > 1;
+		}
+
+		private static bool IsNormalItem(IElement item)
+		{
+			return item.ChildElementCount == 4 && item.FirstElementChild.HasAttribute("colspan");
+		}
+
+		private static bool IsTopItem(IElement item)
+		{
+			return item.ChildElementCount == 5 && item.FirstElementChild.HasAttribute("rowspan");
+		}
+
+		private static bool IsHeader(IElement item)
+		{
+			return item.FirstElementChild.TextContent == "品目";
+		}
+
+		private static bool IsSeparator(IElement item)
+		{
+			return item.ChildElementCount == 1;
+		}
+
+		private static IHtmlDocument GetHtmlDocument(string url)
+		{
+			// 指定したサイトのHTMLをストリームで取得する
+			var doc = default(IHtmlDocument);
+			using (var client = new HttpClient())
+			using (var stream = client.GetStreamAsync(new Uri(url)).Result)
+			{
+				// AngleSharp.Parser.Html.HtmlParserオブジェクトにHTMLをパースさせる
+				var parser = new HtmlParser();
+				doc = parser.ParseAsync(stream).Result;
+			}
+
+			return doc;
+		}
+	}
+}
